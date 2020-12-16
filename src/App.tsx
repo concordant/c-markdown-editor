866ebae764ba49235353d05cb52772358d4e7bd9
Ilyas Toumlilt <ilyas@toumlilt.com>
2020-12-16 14:55:12 +0100

    Add React UI with minimal config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CMDEd from './Components/CMDEd';
 
 function App() {
   return (
     <div className="App">
-      Hello!
+      <CMDEd />
     </div>
   );
 }
