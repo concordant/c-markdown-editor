40e04ec5d4b0e5b4124f18f7db886629ce714706
Ilyas Toumlilt <ilyas@toumlilt.com>
2020-12-29 19:05:57 +0100

    WIP: creating a config interface for the markdown module

--- src/Components/CMDEd.tsx
+++ src/Components/CMDEd.tsx
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
-import MDEditor from '@uiw/react-md-editor';
+import MDEditor, { ICommand } from '@uiw/react-md-editor';
 
 const DEFAULT_VALUE: string = "Connecting to service...";
 
+export interface ICMDEdConfig {
+    /**
+     * An array of ICommand, which, each one, contain a commands property. If no commands are specified, the default will be used. Commands are explained in more details here:
+     * https://github.com/uiwjs/react-md-editor/blob/098c0b657300bfbfef83976558ee37f737e842a2/src/commands/index.ts#L20-L29
+     */
+    commands?: ICommand;
+    /**
+     * Can be used to make Markdown Editor focus itself on initialization.
+     */
+    autoFocus?: boolean;
+    /**
+     * This is reset @uiw/react-markdown-preview settings.
+     */
+    //TODO: need to install @uiw/react-markdown-preview  previewOptions?: ReactMarkdown.ReactMarkdownProps;
+}
 export interface CMDEdProps {
     value?: string // set a default value (or DEFAULT_VALUE is unbefined)
 }
