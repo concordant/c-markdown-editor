b2784793799bbb0eccc7bffa52d19f85f80b8ffd
Ilyas Toumlilt <ilyas@toumlilt.com>
2021-01-02 19:15:24 +0100

    Switched to Slate Wysiwyg Editor

--- src/Components/CMDEd.tsx
+++ src/Components/CMDEd.tsx
@@ -1,6 +1,15 @@
-import React, { useMemo, useState } from "react";
-import { createEditor, Node, Element as SlateElement } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
+import React, { useCallback, useMemo, useState } from "react";
+import {
+  createEditor,
+  Node,
+  Element as SlateElement,
+  Editor,
+  Range,
+  Transforms,
+  Point,
+} from "slate";
+import { withHistory } from "slate-history";
+import { Slate, Editable, withReact, ReactEditor } from "slate-react";
 
 //const DEFAULT_VALUE: string = "Welcome to Concordant Mardown Editor!";
 
@@ -20,54 +29,198 @@ export interface CMDEdState {
   //timestamp: number;
 }
 
+/**
+ * Supported Markdown Shortcuts list
+ */
+const SHORTCUTS: { [char: string]: string } = {
+  "*": "list-item",
+  "-": "list-item",
+  "+": "list-item",
+  ">": "block-quote",
+  "#": "heading-one",
+  "##": "heading-two",
+  "###": "heading-three",
+  "####": "heading-four",
+  "#####": "heading-five",
+  "######": "heading-six",
+};
 
+/**
+ * MD Shortcut -> HTML translation
+ * @param param0
+ */
+const Element = ({ attributes, children, element }: {attributes: any, children: any, element: SlateElement}) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "heading-three":
+      return <h3 {...attributes}>{children}</h3>;
+    case "heading-four":
+      return <h4 {...attributes}>{children}</h4>;
+    case "heading-five":
+      return <h5 {...attributes}>{children}</h5>;
+    case "heading-six":
+      return <h6 {...attributes}>{children}</h6>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+/**
+ * MD to Slate translation module
+ */
+const withShortcuts = (editor: Editor & ReactEditor) => {
+  const { deleteBackward, insertText } = editor;
+
+  editor.insertText = (text) => {
+    const { selection } = editor;
+
+    if (text === " " && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection;
+      const block = Editor.above(editor, {
+        match: (n) => Editor.isBlock(editor, n),
+      });
+      const path = block ? block[1] : [];
+      const start = Editor.start(editor, path);
+      const range = { anchor, focus: start };
+      const beforeText = Editor.string(editor, range);
+      const type = SHORTCUTS[beforeText];
+
+      if (type) {
+        Transforms.select(editor, range);
+        Transforms.delete(editor);
+        const newProperties: Partial<SlateElement> = {
+          type,
+        };
+        Transforms.setNodes(editor, newProperties, {
+          match: (n) => Editor.isBlock(editor, n),
+        });
+
+        if (type === "list-item") {
+          const list = { type: "bulleted-list", children: [] };
+          Transforms.wrapNodes(editor, list, {
+            match: (n) =>
+              !Editor.isEditor(n) &&
+              SlateElement.isElement(n) &&
+              n.type === "list-item",
+          });
+        }
+
+        return;
+      }
+    }
+
+    insertText(text);
+  };
+
+  editor.deleteBackward = (...args) => {
+    const { selection } = editor;
+
+    if (selection && Range.isCollapsed(selection)) {
+      const match = Editor.above(editor, {
+        match: (n) => Editor.isBlock(editor, n),
+      });
+
+      if (match) {
+        const [block, path] = match;
+        const start = Editor.start(editor, path);
+
+        if (
+          !Editor.isEditor(block) &&
+          SlateElement.isElement(block) &&
+          block.type !== "paragraph" &&
+          Point.equals(selection.anchor, start)
+        ) {
+          const newProperties: Partial<SlateElement> = {
+            type: "paragraph",
+          };
+          Transforms.setNodes(editor, newProperties);
+
+          if (block.type === "list-item") {
+            Transforms.unwrapNodes(editor, {
+              match: (n) =>
+                !Editor.isEditor(n) &&
+                SlateElement.isElement(n) &&
+                n.type === "bulleted-list",
+              split: true,
+            });
+          }
+
+          return;
+        }
+      }
+
+      deleteBackward(...args);
+    }
+  };
+
+  return editor;
+};
+
+/**
+ * An init value for the demo
+ */
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [
+      {
+        text:
+          'The editor gives you full control over the logic you can add. For example, it\'s fairly common to want to add markdown-like shortcuts to editors. So that, when you start a line with "> " you get a blockquote that looks like this:',
+      },
+    ],
+  },
+  {
+    type: "block-quote",
+    children: [{ text: "A wise quote." }],
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text:
+          'Order when you start a line with "## " you get a level-two heading, like this:',
+      },
+    ],
+  },
+  {
+    type: "heading-two",
+    children: [{ text: "Try it out!" }],
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text:
+          'Try it out for yourself! Try starting a new line with ">", "-", or "#"s.',
+      },
+    ],
+  },
+];
 
 export default function CMDEd(props: CMDEdProps) {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState<Node[]>([
-    {
-      type: "paragraph",
-      children: [{ text: "A line of text in a paragraph" }],
-    },
-  ]);
-
+  const [value, setValue] = useState<Node[]>(initialValue);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const editor = useMemo(
+    () => withShortcuts(withReact(withHistory(createEditor()))),
+    []
+  );
   return (
-    <Slate
-      editor={editor}
-      value={value}
-      onChange={(newValue) => setValue(newValue)}
-    >
-      <Editable />
+    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
+      <Editable
+        renderElement={renderElement}
+        placeholder="Write some markdown..."
+        spellCheck
+        autoFocus
+      />
     </Slate>
   );
 }
-/*
-class CMDEd2 extends Component<CMDEdProps, CMDEdState> {
-  private i: number = 0;
-
-  constructor(props: CMDEdProps) {
-    super(props);
-    this.state = {
-      value:
-        typeof this.props.value === "undefined"
-          ? DEFAULT_VALUE
-          : this.props.value,
-    };
-  }
-
-  public valueChanged(value: string | undefined) {
-    console.log(this.i++ + ":" + value);
-    value = typeof value === "undefined" ? DEFAULT_VALUE : value;
-    this.setState({
-      value: value,
-    });
-  }
-
-  render() {
-
-    const cmdeditor = <div className="mded-container"></div>;
-
-    return cmdeditor;
-  }
-}
-*/
